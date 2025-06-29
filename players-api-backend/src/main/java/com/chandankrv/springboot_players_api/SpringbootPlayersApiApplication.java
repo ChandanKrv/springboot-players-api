d37760654f14775703c2e40733918142931ea45a
ChandanKrv <chandan.nit.cse@gmail.com>
2025-06-29 11:04:43 +0530

    initial commit

---
+++ players-api-backend/src/main/java/com/chandankrv/springboot_players_api/SpringbootPlayersApiApplication.java
@@ -0,0 +1,13 @@
+package com.chandankrv.springboot_players_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPlayersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPlayersApiApplication.class, args);
+	}
+
+}
---
+++ players-api-backend/src/test/java/com/chandankrv/springboot_players_api/SpringbootPlayersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandankrv.springboot_players_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPlayersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
